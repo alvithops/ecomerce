08123f1cf6fb214eb3083ead4eb6550903e7dee9
alvithops <alvithops@gmail.com>
2026-02-09 13:12:25 +0700

    Fix incorrect redirect paths in logout.php

--- public/logout.php
+++ public/logout.php
@@ -4,9 +4,9 @@ $isAdmin = isset($_SESSION['admin_logged_in']);
 session_destroy();
 
 if ($isAdmin) {
-    header("Location: public/login_admin.php");
+    header("Location: login_admin.php");
 } else {
-    header("Location: public/login_pemesan.php");
+    header("Location: login_pemesan.php");
 }
 exit();
 ?>
